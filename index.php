57ed32e6548cbc40590a2afc0a4c27115d2957b0
vmartinezc5 <110207678+vmartinezc5@users.noreply.github.com>
2022-09-05 22:29:44 -0600

    Finalmente

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><center><h3>PAGINA DE INICIO </h3></center></p>
+
+    <p>Victor Emmanuel.... Prueba de Sincronización</p>
+
+    
+    <a href="VistaEstudiante.php"> Ir a Vista Estudiante</a>
+
+
+    
+
+
+</body>
+</html>
